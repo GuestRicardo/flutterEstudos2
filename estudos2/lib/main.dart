89b7487c39b64c884e9e65110df6f9710c67d6ee
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-12-09 16:32:03 -0300

    formatanbdo

--- estudos2/lib/main.dart
+++ estudos2/lib/main.dart
@@ -3,6 +3,16 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 
 void main() async {
+  //a busca
   Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
   final future = http.get(uri);
+  //resultado
+  future.then((response) => {
+    if(response.statusCode == 200){
+     print('pagina carregada com sucesso') 
+    } else {
+      print('erro ao carregar a pagina')
+    }
+
+  });
 }
