461a8c0e45e79c0e447c5f380a79e8a4714f61e4
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-12-17 21:13:24 -0300

    formatando

--- estudos2/lib/main.dart
+++ estudos2/lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:developer';
 
 void main() async {
   //a busca
-  Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
+  Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/');
   final future = http.get(uri);
   //resultado
   future.then((response) => {
-    if(response.statusCode == 200){
-     print('pagina carregada com sucesso') 
-    } else {
-      print('erro ao carregar a pagina')
-    }
-
-  });
+        if (response.statusCode == 200){
+            print(json.decode(response.body)),            
+            var list = json.decode(response.body) as List,
+            list.forEach((element) {
+              print(element['title']);
+            }),
+          }else{
+            print('erro ao carregar a pagina')}
+      });
 }
