0d1b8dad8dcb210a32e7ebeb62a1335ca354108c
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2024-12-17 21:44:35 -0300

    mapeando lista do json

--- estudos2/lib/main.dart
+++ estudos2/lib/main.dart
@@ -6,16 +6,22 @@ import 'dart:developer';
 
 void main() async {
   //a busca
-  Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/');
+  Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
+  print(uri);
   final future = http.get(uri);
   //resultado
   future.then((response) => {
         if (response.statusCode == 200){
-            print(json.decode(response.body)),            
-            var list = json.decode(response.body) as List,
-            list.forEach((element) {
-              print(element['title']);
-            }),
+          print('pagina carregada com sucesso!'),
+
+            print(json.decode(response.body)),    
+
+            Map<String, dynamic> dados = json.decode(response.body),
+           print(dados),
+            // var list = json.decode(response.body) as List,
+            // list.forEach((element) {
+            //   print(element['title']);
+            // }),
           }else{
             print('erro ao carregar a pagina')}
       });
